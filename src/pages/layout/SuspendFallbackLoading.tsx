d31e6ccc7b0402cd149a7939121d9b40df226cbe
liaojiayun <liaojiayun@moresales.cn>
2021-08-21 17:49:42 +0800

    feat: 优化Suspend组件

--- src/pages/layout/SuspendFallbackLoading.tsx
+++ src/pages/layout/SuspendFallbackLoading.tsx
@@ -3,13 +3,7 @@ import {Spin, Alert} from "antd";
 import React from "react";
 import "./index.less";
 
-interface FallbackMessageProps {
-    message: string
-
-    description?: string
-}
-
-const SuspendFallbackLoading: FC<FallbackMessageProps> = ({message, description}) => {
+const SuspendFallbackLoading: FC = () => {
     return (<div className="loading__wrapper">
         <Spin size="large" className="loading" />
     </div>)
